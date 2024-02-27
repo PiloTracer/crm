afa44774dea0ab839cc0adf0b9df0012e2dd52c4
ACS PiloTracer <acsking2022@gmail.com>
2024-02-26 21:39:55 -0600

    researching

--- dirs.sh
+++ dirs.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-#this actually does nothing,
-#remember the dirs are at ~/crm/uploads/...
 
-sudo mkdir -p /crmdir/uploads/fast/log
-sudo mkdir -p /crmdir/uploads/fast/files
-sudo mkdir -p /crmdir/uploads/pytap/log
-sudo mkdir -p /crmdir/uploads/sock/log
+sudo mkdir -p ~/crm/uploads/fast/log
+sudo mkdir -p ~/crm/uploads/fast/files
+sudo mkdir -p ~/crm/uploads/pytap/log
+sudo mkdir -p ~/crm/uploads/sock/log
 
-sudo chmod 777 /crmdir/uploads/fast/log
-sudo chmod 777 /crmdir/uploads/fast/files
-sudo chmod 777 /crmdir/uploads/pytap/log
-sudo chmod 777 /crmdir/uploads/sock/log
+sudo chmod 777 ~/crm/uploads/fast/log
+sudo chmod 777 ~/crm/uploads/fast/files
+sudo chmod 777 ~/crm/uploads/pytap/log
+sudo chmod 777 ~/crm/uploads/sock/log
 
 sudo chmod +x binsh/PRD/*
 
